a6eafec300bb25d50a866bd27f3466fcdb370140
Shubhamdutta2000 <dondutta2000@gmail.com>
2020-08-15 10:59:31 +0530

    Updates

---
+++ precache-manifest.cad121731e459e60a7cb81ecf674d25e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b130f2a1a139eda1cc462d4abfaf53",
+    "url": "/SDKeeperApp/index.html"
+  },
+  {
+    "revision": "f7eecfdc5b1b85ae00b5",
+    "url": "/SDKeeperApp/static/js/2.1acdfabb.chunk.js"
+  },
+  {
+    "revision": "c6d4a78147bd427ad6a9",
+    "url": "/SDKeeperApp/static/js/main.8ab0b5a2.chunk.js"
+  },
+  {
+    "revision": "a0d58e1914683e052c16",
+    "url": "/SDKeeperApp/static/js/runtime-main.dae7a925.js"
+  }
+]);
